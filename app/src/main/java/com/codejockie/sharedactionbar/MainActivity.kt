57ef7bd890d15c1c2934bc1587e7ec9004a506bb
Kennedy Nwaorgu <devjckennedy@gmail.com>
2023-11-01 13:31:20 -0400

    add actions to top app bar; update compile sdk version

--- app/src/main/java/com/codejockie/sharedactionbar/MainActivity.kt
+++ app/src/main/java/com/codejockie/sharedactionbar/MainActivity.kt
@@ -3,11 +3,30 @@ package com.codejockie.sharedactionbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.codejockie.sharedactionbar.ui.theme.SharedActionBarTheme
@@ -28,9 +47,75 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlaygroundApp() {
+    Scaffold(
+        topBar = {
+            HomeTopAppBar(
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = "App content"
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar(
+    modifier: Modifier = Modifier,
+) {
+    var menuExpanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = {
+            Text(
+                text = "My App"
+            )
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Outlined.Search, contentDescription = "Search")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Outlined.FavoriteBorder, contentDescription = "Favourite")
+            }
+            IconButton(onClick = { menuExpanded = menuExpanded.not() }) {
+                Icon(Icons.Outlined.MoreVert, contentDescription = "More")
+            }
+            DropdownMenu(expanded = menuExpanded, onDismissRequest = { menuExpanded = false }) {
+                DropdownMenuItem(text = { Text(text = "Refresh") }, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Settings") }, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "About") }, onClick = { /*TODO*/ })
+            }
+        },
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeTopAppBarPreview() {
     SharedActionBarTheme {
+        HomeTopAppBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlaygroundAppPreview() {
+    SharedActionBarTheme {
+        PlaygroundApp()
     }
 }
